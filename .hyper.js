6fd744a7edb5318d9a1fd645827a70137ffc1b4f
yasainet <takumi.mizoguchi@gmail.com>
2024-10-07 12:39:12 +0900

    feat(.hyper.js): hyper-searchプラグインを追加 新機能(.hyper_plugins/package.json): hyper-searchの依存関係を追加

--- .hyper.js
+++ .hyper.js
@@ -143,7 +143,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hypurr#latest", "hyper-opacity", "hypercwd"],
+  plugins: ["hypurr#latest", "hyper-opacity", "hypercwd", "hyper-search"],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
