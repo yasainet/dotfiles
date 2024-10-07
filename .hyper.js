eea5841dacd4895dee29580ba1d3c02e30602c26
yasainet <takumi.mizoguchi@gmail.com>
2024-10-07 12:27:34 +0900

    修正(.hyper.js): 不要なhyper-tokyo-nightプラグインを削除 修正(package.json): hyper-tokyo-nightの依存関係を削除

--- .hyper.js
+++ .hyper.js
@@ -143,7 +143,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hypurr#latest", "hyper-opacity", "hypercwd", "hyper-tokyo-night"],
+  plugins: ["hypurr#latest", "hyper-opacity", "hypercwd"],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
