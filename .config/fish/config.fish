7e87054788840fdc66237ea7085b78392066325a
yasainet <takumi.mizoguchi@gmail.com>
2025-02-25 06:43:58 +0900

    修正(fish config): y関数をya関数にリネームし、cwdオプションを削除 追加(yazi.toml): yaziの設定ファイルを新規作成

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -79,13 +79,8 @@ function cd
 end
 
 # yazi
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+function ya
+  yazi $argv
 end
 
 # PATH
