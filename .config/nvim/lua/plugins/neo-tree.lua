b00a6258ff71632e4d49bb755ce7c1ae958f78ff
yasainet <takumi.mizoguchi@gmail.com>
2025-06-21 13:41:53 +0900

    機能(neo-tree.lua): neo-treeプラグインの設定を追加 設定を拡張し、ファイルシステムのオプションやウィンドウのマッピングを定義

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,5 +7,63 @@ return {
     "MunifTanjim/nui.nvim",
   },
   lazy = false,
-  opts = {},
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false
+      },
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = true,
+        hide_gitignored = true,
+        hide_by_name = {
+          "node_modules",
+          ".git",
+          ".DS_Store"
+        },
+        always_show = {
+          ".gitignore"
+        },
+        always_show_by_pattern = {
+          ".env*"
+        }
+      }
+    },
+    use_libuv_file_watcher = true,
+    window = {
+      width = 30,
+      position = "left",
+      auto_expand_width = true,
+      mappings = {
+        ["H"] = "toggle_hidden",
+        ["S"] = "open_split",
+        ["s"] = "open_vsplit",
+        ["a"] = "add",
+        ["A"] = "add_directory",
+        ["d"] = "delete",
+        ["r"] = "rename",
+        ["y"] = "copy_to_clipboard",
+        ["R"] = "refresh",
+        ["/"] = "fuzzy_finder",
+      }
+    },
+    close_if_last_window = false,
+    enable_git_status = true,
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          added = "+",
+          modified = "~",
+          deleted = "-",
+          renamed = "»",
+          untracked = "?",
+          ignored = "◌",
+          unstaged = "●",
+          staged = "✓",
+          conflict = "!"
+        }
+      }
+    }
+  },
 }
