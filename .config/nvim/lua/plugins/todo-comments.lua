e0c888cd7da5cd42f2766ac96e878065213cbf05
yasainet <takumi.mizoguchi@gmail.com>
2026-02-04 12:30:13 +0900

    update

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,8 +1,16 @@
 return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("todo-comments").setup()
-	end,
+	opts = {
+		highlight = {
+			comments_only = false, -- match outside of comments (for markdown callouts)
+			pattern = {
+				[[.*<(KEYWORDS)\s*:]], -- default: TODO:
+				[=[.*>\s*\[!(KEYWORDS)\]]=], -- obsidian: > [!TODO]
+			},
+		},
+		search = {
+			pattern = [=[\b(KEYWORDS):|>\s*\[!(KEYWORDS)\]]=], -- both patterns
+		},
+	},
 }
-
