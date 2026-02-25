9b65b00c3222233b0c535353a77bc3b7ce3715c6
yasainet <mail@yasainet.com>
2026-02-26 02:52:35 +0900

    update

--- config/nvim/lua/plugins/claudecode.lua
+++ config/nvim/lua/plugins/claudecode.lua
@@ -1,36 +1,17 @@
 return {
 	"coder/claudecode.nvim",
 	dependencies = { "folke/snacks.nvim" },
+	cond = function()
+		return #vim.api.nvim_list_uis() > 0
+	end,
 	config = function()
 		require("claudecode").setup({
 			diff_opts = {
 				auto_close_on_accept = true,
-				keep_terminal_focus = true,
+				vertical_split = true,
 			},
 			terminal = {
-				split_side = "right",
-				split_width_percentage = 0.4,
-				provider = "snacks",
-				snacks_win_opts = {
-					keys = {
-						nav_h = {
-							"<C-h>",
-							function()
-								vim.cmd("wincmd h")
-							end,
-							mode = "t",
-							desc = "Go to left window",
-						},
-						nav_l = {
-							"<C-l>",
-							function()
-								vim.cmd("wincmd l")
-							end,
-							mode = "t",
-							desc = "Go to right window",
-						},
-					},
-				},
+				provider = "none",
 			},
 		})
 	end,
