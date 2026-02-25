7f0c9361007120c929f8dc416839cf32334386bb
yasainet <mail@yasainet.com>
2026-02-25 22:42:11 +0900

    update

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -84,7 +84,7 @@ vim.keymap.set("n", "<leader>bc", function()
 	local current = vim.api.nvim_get_current_buf()
 	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 		if buf ~= current and vim.bo[buf].buftype == "" and vim.api.nvim_buf_is_loaded(buf) then
-			vim.api.nvim_buf_delete(buf, {})
+			vim.api.nvim_buf_delete(buf, { force = true })
 		end
 	end
 	print("Buffers cleared")
