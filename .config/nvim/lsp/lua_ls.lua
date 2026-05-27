9eb7d54b3442fcc46fcb91c521c38882004498a6
yasainet <mail@yasainet.com>
2026-05-27 21:34:03 +0900

    update

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -4,18 +4,8 @@ return {
 	root_markers = { ".luarc.json", ".luarc.jsonc", ".stylua.toml", "stylua.toml", ".git" },
 	settings = {
 		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim", "Snacks" },
-			},
 			workspace = {
 				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			completion = {
-				callSnippet = "Replace",
 			},
 		},
 	},
