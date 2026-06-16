c3f408900e106faafeaa4f5b18103f5c2fdf74e5
yasainet <mail@yasainet.com>
2026-06-16 10:12:44 +0900

    feat(tmux): fd/rg + fzf でファイル選択 popup を M-f に追加

---
+++ .config/tmux/scripts/grep-preview.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+query="$1"
+file="$2"
+
+if [[ -z $query || -z $file ]]; then
+  bat --color=always --style=numbers "$file" 2>/dev/null || cat "$file"
+  exit 0
+fi
+
+mapfile -t lines < <(rg --line-number --no-heading --color=never -I -- "$query" "$file" 2>/dev/null | cut -d: -f1 | head -20)
+
+if [[ ${#lines[@]} -eq 0 ]]; then
+  bat --color=always --style=numbers "$file" 2>/dev/null || cat "$file"
+  exit 0
+fi
+
+hl_flags=()
+for l in "${lines[@]}"; do
+  hl_flags+=(--highlight-line "$l")
+done
+
+first_line="${lines[0]}"
+start=$((first_line - 10))
+[[ $start -lt 1 ]] && start=1
+
+bat --color=always --style=numbers,header --line-range "$start:" --highlight-line "${lines[0]}" "${hl_flags[@]}" "$file" 2>/dev/null || cat "$file"
