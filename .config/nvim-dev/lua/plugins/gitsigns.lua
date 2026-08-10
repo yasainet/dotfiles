3f56116c8bc551855950fc6bfe7d5debdd087afc
yasainet <mail@yasainet.com>
2026-08-10 12:42:34 +0900

    change(nvim-dev): gitsigns の記号を nvim に揃える
    add, change, changedelete, untracked を █ にして、signcolumn の
    変更箇所を旧 nvim 設定と同じ塊で見えるようにする。

--- .config/nvim-dev/lua/plugins/gitsigns.lua
+++ .config/nvim-dev/lua/plugins/gitsigns.lua
@@ -3,12 +3,20 @@ return {
   lazy = false,
   opts = {
     signs = {
+      add = { text = "█" },
+      change = { text = "█" },
       delete = { text = "" },
       topdelete = { text = "" },
+      changedelete = { text = "█" },
+      untracked = { text = "█" },
     },
     signs_staged = {
+      add = { text = "█" },
+      change = { text = "█" },
       delete = { text = "" },
       topdelete = { text = "" },
+      changedelete = { text = "█" },
+      untracked = { text = "█" },
     },
   },
 }
